d709d369ee95c8fd9f0c06bbd8bbf7874430bea3
Yeison Delgado <delgadoyeisonfer@gmail.com>
2024-12-03 17:10:31 -0500

    cambio

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-{
-  "SecurityKeys": {
-    "StripeApiKey": "sk_test_51H8xx4eC39HqlyjwDarjtT1zd7dc",
-    "JwtSecretKey": "my_super_secret_key_for_testing_12345",
-    "GoogleApiKey": "AIzaSyD_test_google_api_key_98765"
-  }
-}
+string stripe_api_key="sk_test_4eC39HqlyjwDarjtT1zd7dc";
